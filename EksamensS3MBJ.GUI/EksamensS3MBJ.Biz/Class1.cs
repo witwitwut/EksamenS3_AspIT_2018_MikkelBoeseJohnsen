8aab8b26dce6fc634dd5051e1206b99383e4b17d
Mikkel Boese Johnsen <mikk571a@edu.campusvejle.dk>
2018-11-12 08:56:32 +0100

    Startede ud med at opsætte IO og indsætte AsynchronousClient til at kunne hente data fra en asynchron Database

--- EksamensS3MBJ.GUI/EksamensS3MBJ.Biz/Class1.cs
+++ EksamensS3MBJ.GUI/EksamensS3MBJ.Biz/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EksamensS3MBJ.IO
+namespace EksamensS3MBJ.Biz
 {
     public class Class1
     {
---
+++ EksamensS3MBJ.GUI/EksamensS3MBJ.IO/StateObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksamensS3MBJ.IO
+{
+    public class StateObject
+    {
+        // Client socket.  
+        public Socket workSocket = null;
+        // Size of receive buffer.  
+        public const int BufferSize = 4096;
+        // Receive buffer.  
+        public byte[] buffer = new byte[BufferSize];
+        // Received data string.  
+        public StringBuilder sb = new StringBuilder();
+    }
+}
