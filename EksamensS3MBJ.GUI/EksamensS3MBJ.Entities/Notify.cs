7a5f4588a0257f0eb443dce21d207747f70dcefd
Mikkel Boese Johnsen <mikk571a@edu.campusvejle.dk>
2018-11-12 09:56:19 +0100

    Lavede en Notify Klasse som skal genopfriske de informationer jeg putter op i min WPF/GUI og en tom UserLogin som skal bruges til at tjekke at vedkommende der logger ind ikke har adgang til bruger oplsyningen før han er logget ind i system(tror det er sådan at man kan forstå det)

---
+++ EksamensS3MBJ.GUI/EksamensS3MBJ.Entities/Notify.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksamensS3MBJ.Entities
+{
+    public class Notify : INotifyPropertyChanged
+    {
+        public Notify(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
--- EksamensS3MBJ.GUI/EksamensS3MBJ.Entities/UserLogin.cs
+++ EksamensS3MBJ.GUI/EksamensS3MBJ.Entities/UserLogin.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EksamensS3MBJ.Entities
 {
-    public class Class1
+    class UserLogin
     {
     }
 }
